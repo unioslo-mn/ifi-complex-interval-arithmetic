107e990addb0ddb36c3b5f6d92863f98f67e1e96
gaborge <gaborge@ulrik.uio.no>
2024-01-08 15:23:42 +0100

    Implemented Edge and Arc classes needed for the polyarcular interval operations.

---
+++ +ciat/Arc.m
@@ -0,0 +1,116 @@
+classdef Arc
+
+	properties
+        Center          % Arc center as a complex number
+        Radius          % Arc radius as a real number (negative means concave arc, zero means vertex)
+    	Angle			% Arc angle as a real interval (always counter-clockwise)
+	end	
+
+	properties (Dependent)
+		Endpoints		% Endpoints of the arc as complex numbers
+		Length			% Length of the arc curve
+		GaussMap        % Gauss map of the curve as real interval
+		LogGaussMap     % Gauss map of the log curve as real interval
+		NormFactor		% Normalization factor to a real 1 centered arc (zero is zero centered)
+		CurveParameter	% Parameter interval of the normalized curve as real interval
+	end
+
+	methods
+		%% Constructor
+        function obj = Arc(center,radius,angle)
+            mustBeA(center,'double')
+            mustBeA(radius,'double')
+            mustBeA(angle,'ciat.RealInterval')
+
+            obj.Center = center;
+            obj.Radius = radius;
+            obj.Angle = ciat.RealInterval(ciat.wrapPi(angle.Bounds));
+		end
+
+		%% Defining properties
+        
+        % Endpoints
+		function value = get.Endpoints(obj)
+            value = obj.Center + obj.Radius * exp(1i*obj.Angle.Bounds);
+        end
+
+        % Length
+        function value = get.Length(obj)
+            value = 2*pi*obj.Radius * obj.Angle.Width/(2*pi);
+        end
+
+        % Gauss map angle interval
+        function value = get.GaussMap(obj)
+            value = obj.Angle;
+        end
+
+        % Log-Gauss map angle interval
+        function value = get.LogGaussMap(obj)
+            value = obj.GaussMap - ciat.RealInterval(angle(obj.Endpoints));
+        end
+
+        % Normalization factor
+        function value = get.NormFactor(obj)
+            if obj.Center == 0
+                value = 0;
+            else
+                value = 1/obj.Center;
+            end
+        end
+
+        % Curve parameter
+        function value = get.CurveParameter(obj)
+            value = obj.Angle + angle(obj.NormFactor);
+        end
+
+		%% Other methods
+
+        % Plot
+        function h = plot(obj, varargin)
+        % Plot polygonal intervals 
+        %
+        % This function plots a set of polygonal intervals 
+        % (see MATLAB plot function)
+        % _________________________________________________________________________
+        % USAGE        
+        %   r = plot(obj)
+        % _________________________________________________________________________
+        % NECESSARY ARGUMENT
+        %   obj       : array of objects from the ciat.Arc class
+        % _________________________________________________________________________
+        % OPTIONS
+        % _________________________________________________________________________
+        % EXAMPLES
+        %   
+        % _________________________________________________________________________
+            tf = ishold;
+            hold on
+            h = [];
+            for n = 1:length(obj(:))
+                % Extract parameters
+                cr = real(obj.Center);
+                ci = imag(obj.Center);
+                r = obj.Radius;
+                % Plot arc by angle quadrants
+                q = [-pi,-pi/2 ; -pi/2,0 ; 0,pi/2 ; pi/2,pi];
+                for idx = 1:4
+                    qi = intersection([obj.Angle, ...
+                                        ciat.RealInterval(q(idx,:))]);
+                    if ~isempty(qi)
+                        xBound = sort(cr + r*cos(qi.Bounds) );
+                        yBound = sort(ci + r*sin(qi.Bounds) );
+                        h = [h; ...
+                            fimplicit(@(x,y) (x-cr).^2 + (y-ci).^2 - r^2 , ...
+                                        [xBound yBound],varargin{:})];    
+                    end
+                end
+            end
+            if tf == false 
+                hold off
+            end
+        end
+
+	end
+
+
+end
---
+++ +ciat/Edge.m
@@ -0,0 +1,112 @@
+classdef Edge
+
+	properties
+        Endpoints       % Endpoints of the edge as complex numbers
+    end	
+
+	properties (Dependent)
+        Vector          % Endpoint difference as a complex vector
+        Slope           % Slope of the line the edge fits on
+        ZeroCrossing    % Real axis crossing point of the line the edge fits on
+		Length			% Length of the edge curve
+		GaussMap        % Gauss map of the curve as real interval
+		LogGaussMap     % Gauss map of the log curve as real interval
+		NormFactor		% Normalization factor to a real 1 crossing vertical edge (zero is zero crossing)
+		CurveParameter	% Parameter interval of the normalized curve as real interval
+	end
+
+	methods
+		%% Constructor
+        function obj = Edge(endpoints)
+            mustBeA(endpoints,'double')
+
+            obj.Endpoints = endpoints;
+		end
+
+		%% Defining properties
+
+        % Vector
+        function value = get.Vector(obj)
+            value = diff(obj.Endpoints);
+        end
+
+        % Slope
+		function value = get.Slope(obj)
+            vd = obj.Vector;
+            value = imag(vd)/real(vd);
+        end
+
+        % Zero crossing
+		function value = get.ZeroCrossing(obj)
+            v1 = obj.Endpoints(1);
+            value = real(v1) - imag(v1) / obj.Slope;
+        end
+
+        % Length
+		function value = get.Length(obj)
+            value = abs(obj.Vector);
+        end
+
+        % GaussMap
+		function value = get.GaussMap(obj)
+            value = ciat.RealInterval(ciat.wrapPi(angle(obj.Vector)-pi/2));
+        end
+
+        % Log-GaussMap
+		function value = get.LogGaussMap(obj)
+            value = obj.GaussMap - ciat.RealInterval(angle(obj.Endpoints));
+        end
+
+        % Normalization factor
+		function value = get.NormFactor(obj)
+            if obj.ZeroCrossing == 0
+                value = 0;
+            else
+                % Extract line parameters
+                v1 = obj.Endpoints(1);
+                va = obj.Slope;        
+                
+                % Find scale-rotate factor
+                vr = exp(-1i*(atan(va)+pi/2));   % rotation factor
+                vs = 1/real(vr * v1);       % scale factor
+                value = vr * vs;                        % complex factor
+
+            end
+        end
+
+        %% Other methods
+
+        % Plot
+        function h = plot(obj, varargin)
+        % Plot polygonal intervals 
+        %
+        % This function plots a set of polygonal intervals 
+        % (see MATLAB plot function)
+        % _________________________________________________________________________
+        % USAGE        
+        %   r = plot(obj)
+        % _________________________________________________________________________
+        % NECESSARY ARGUMENT
+        %   obj       : array of objects from the ciat.Arc class
+        % _________________________________________________________________________
+        % OPTIONS
+        % _________________________________________________________________________
+        % EXAMPLES
+        %   
+        % _________________________________________________________________________
+            tf = ishold;
+            hold on
+            h = [];
+            for n = 1:length(obj(:))
+                points = obj(n).Endpoints;
+                h = [h;plot(real(points), imag(points), varargin{:})];    
+            end
+            if tf == false 
+                hold off
+            end
+        end
+
+    end
+
+
+end
